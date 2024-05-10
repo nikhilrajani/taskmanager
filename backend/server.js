1134236b1a900407a96e26b298adbfedcc4ebbe1
Nikhil <nikhilrajani309@gmail.com>
2024-05-11 01:07:17 +0530

    Backend updates

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from "express";
 import mysql from "mysql";
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app=express();
 
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Nik.Raj9003",
-    database:"taskmanager"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
 })
 
 db.connect(function(err){
